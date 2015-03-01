18c767d953760f395c00cf9cc0f8e4f8c2e33603
algoquant <algoquant@algoquants.ch>
2015-03-01 14:16:11 -0500

    commit 03-01-15
    added roxygen doumentation

--- R/HighFreq.R
+++ R/HighFreq.R
@@ -1,24 +1,17 @@
-################################################
-###
-###  scrubbing and aggregating HFREQ data
-###
-################################################
-
-
-###########
-# functions
-
-
 ### recursively "rbind" a list of xts time series - same as do.call.rbind
 # call lapply in a loop to divide list_var by half, binding neighboring elements
 #' Recursively "rbind" a list of objects.
 #' 
-#' Performs the same operation as \code{do.call(rbind, list_var)}, but using recursion, which is much faster and uses less memory.
-#' This is the same function as '\code{do.call.rbind}' from package 'qmao'.
+#' Performs the same operation as \code{do.call(rbind, list_var)}, but using
+#' recursion, which is much faster and uses less memory. This is the same
+#' function as '\code{\link[qmao]{do.call.rbind}}' from package 'qmao'.
 #' 
-#' @param list_var A list of \code{vectors}, \code{matrices}, \code{data frames}, or \code{time series}.
-#' @return  a single \code{vector}, \code{matrix}, \code{data frame}, or \code{time series}.
-#' @details Performing a \code{do.call(rbind, x_ts)} on a list of 'xts' time series is very slow, and often causes an "out of memory" error.
+#' @param list_var A list of \code{vectors}, \code{matrices}, \code{data
+#'   frames}, or \code{time series}.
+#' @return  a single \code{vector}, \code{matrix}, \code{data frame}, or
+#'   \code{time series}.
+#' @details Performing a \code{do.call(rbind, x_ts)} on a list of 'xts' time
+#'   series is very slow, and often causes an "out of memory" error.
 #' @examples
 #' # create xts time series
 #' x_ts <- xts(x=rnorm(1000), order.by=(Sys.time()-3600*(1:1000)))
@@ -43,6 +36,7 @@ do_call_rbind <- function(list_var) {
 }  # end do_call_rbind
 
 
+
 ### identify extreme values in a univariate xts time series
 #' Identify extreme values in a univariate xts time series.
 #' 
@@ -53,8 +47,10 @@ do_call_rbind <- function(list_var) {
 #' @param vol_window number of data points for estimating running volatility.
 #' @param thresh_old level of prices above which values are considered extreme.
 #' @return  \code{logical vector}.
-#' @details Extreme values are those that exceed a fixed multiple of the running volatility.
-#' @details Extreme values are the very tips of the tails when the distribution of values becomes very fat-tailed.
+#' @details Extreme values are those that exceed a fixed multiple of the running
+#'   volatility.
+#' @details Extreme values are the very tips of the tails when the distribution
+#'   of values becomes very fat-tailed.
 #' @examples
 #' # create xts time series
 #' x_ts <- xts(x=rnorm(1000), order.by=(Sys.time()-3600*(1:1000)))
@@ -63,7 +59,8 @@ do_call_rbind <- function(list_var) {
 extreme_values <- function(time_series, vol_window=51, thresh_old=2) {
 
 # calculate volatility as running quantile
-  vo_lat <- runquantile(x=abs(as.vector(time_series)), k=vol_window, probs=0.9, endrule="constant", align="center")
+  vo_lat <- runquantile(x=abs(as.vector(time_series)), k=vol_window, 
+                        probs=0.9, endrule="constant", align="center")
   vo_lat <- xts(vo_lat, order.by=index(time_series))
   colnames(vo_lat) <- "volat"
 # carry forward non-zero volatility values
@@ -86,15 +83,19 @@ extreme_values <- function(time_series, vol_window=51, thresh_old=2) {
 ### identify jump values in a univariate xts price time series
 #' Identify jump values in a univariate xts time series.
 #' 
-#' Calculates the running volatility of simple returns, defined as a quantile over a sliding window.
-#' Identifies jump values as those returns that exceed the scaled running volatility, but their sum with the lagged returns doesn't exceed it.
+#' Calculates the running volatility of simple returns, defined as a quantile
+#' over a sliding window. Identifies jump values as those returns that exceed
+#' the scaled running volatility, but their sum with the lagged returns doesn't
+#' exceed it.
 #' 
 #' @param time_series univariate \code{xts} time series.
-#' @param vol_window number of data points for estimating running volatility.
-#' @param thresh_old level of change above which values are considered extreme.
+#' @inheritParams extreme_values
 #' @return  \code{logical vector}.
-#' @details Jump values are those where neighboring values both exceed the scaled running volatility (defined as a quantile), but their sum doesn't exceed it.
-#' @details Jump values are simply isolated prices out of proportion to their neighbors.
+#' @details Jump values are those where neighboring values both exceed the
+#'   scaled running volatility (defined as a quantile), but their sum doesn't
+#'   exceed it.
+#' @details Jump values are simply isolated prices out of proportion to their
+#'   neighbors.
 #' @examples
 #' # create xts time series
 #' x_ts <- xts(x=rnorm(1000), order.by=(Sys.time()-3600*(1:1000)))
@@ -111,7 +112,8 @@ jump_values <- function(time_series, vol_window=51, thresh_old=2) {
   colnames(diff_series_fut) <- "diff_series_fut"
 
 # calculate vo_lat as running quantile
-  vo_lat <- runquantile(x=abs(as.vector(diff_series)), k=vol_window, probs=0.9, endrule="constant", align="center")
+  vo_lat <- runquantile(x=abs(as.vector(diff_series)), k=vol_window, 
+                        probs=0.9, endrule="constant", align="center")
   vo_lat <- xts(vo_lat, order.by=index(diff_series))
   colnames(vo_lat) <- "volat"
 # carry forward non-zero vo_lat values
@@ -140,15 +142,17 @@ jump_values <- function(time_series, vol_window=51, thresh_old=2) {
 # return mid price and volume
 #' Scrub a single day of TAQ data and aggregate to OHLC format.
 #' 
-#' Calculate mid prices from bid and offer TAQ data, scrub them and cbind with volume data, and aggregate to 'period' OHLC data.
+#' Calculate mid prices from bid and offer TAQ data, scrub them and cbind with
+#' volume data, and aggregate to 'period' OHLC data.
 #' 
 #' @param taq_data \code{TAQ xts} time series.
-#' @param vol_window number of data points for estimating running volatility.
-#' @param thresh_old level of change above which values are extreme.
+#' @inheritParams extreme_values
 #' @param period aggregation period.
 #' @return  \code{OHLC xts} time series.
-#' @details Valid 'period' character strings include: "minutes", "3 min", "5 min", "10 min", "15 min", "30 min", and "hours".
-#' @details The time index is rounded up to the next integer multiple of 'period'.
+#' @details Valid 'period' character strings include: "minutes", "3 min", "5
+#'   min", "10 min", "15 min", "30 min", and "hours".
+#' @details The time index is rounded up to the next integer multiple of
+#'   'period'.
 #' @examples
 #' # create time index of one second intervals for a single day
 #' in_dex <- seq(from=as.POSIXct("2015-02-09 09:30:00"), to=as.POSIXct("2015-02-09 16:00:00"), by="1 sec")
@@ -169,7 +173,7 @@ jump_values <- function(time_series, vol_window=51, thresh_old=2) {
 scrub_agg <- function(taq_data, vol_window=51, thresh_old=2, period="minutes") {
 
 # convert timezone of index to New_York
-  index(taq_data) <- with_tz(index(taq_data), "America/New_York")
+  index(taq_data) <- with_tz(time=index(taq_data), tzone="America/New_York")
 # subset data to NYSE trading hours
   taq_data <- taq_data['T09:30:00/T16:00:00', ]
 # return NULL if no data
@@ -180,7 +184,7 @@ scrub_agg <- function(taq_data, vol_window=51, thresh_old=2, period="minutes") {
   taq_data <- taq_data[!duplicated(index(taq_data)), ]
 
 # scrub quotes with suspect bid-offer spreads
-  bid_offer <- taq_data[, 'Ask.Price'] - taq_data[, 'Bid.Price']
+  bid_offer <- taq_data[, "Ask.Price"] - taq_data[, "Bid.Price"]
 #  bid_offer <- na.omit(bid_offer)
   sus_pect <- extreme_values(bid_offer, vol_window=vol_window, thresh_old=thresh_old)
 # remove suspect values
@@ -223,11 +227,13 @@ scrub_agg <- function(taq_data, vol_window=51, thresh_old=2, period="minutes") {
 #' Scrub a single day of TAQ data in xts format.
 #' 
 #' @param taq_data \code{TAQ xts} time series.
-#' @param vol_window number of data points for estimating running volatility.
-#' @param thresh_old level of change above which values are extreme.
+#' @inheritParams extreme_values
 #' @return  \code{TAQ xts} time series.
-#' @details Jump values are those where neighboring values both exceed the scaled running volatility (defined as a quantile), but their sum doesn't exceed it.
-#' @details Jump values are simply isolated prices out of proportion to their neighbors.
+#' @details Jump values are those where neighboring values both exceed the
+#'   scaled running volatility (defined as a quantile), but their sum doesn't
+#'   exceed it.
+#' @details Jump values are simply isolated prices out of proportion to their
+#'   neighbors.
 #' @examples
 #' # create time index of one second intervals for a single day
 #' in_dex <- seq(from=as.POSIXct("2015-02-09 09:30:00"), to=as.POSIXct("2015-02-09 16:00:00"), by="1 sec")
@@ -247,17 +253,20 @@ scrub_agg <- function(taq_data, vol_window=51, thresh_old=2, period="minutes") {
 scrub_TAQ <- function(taq_data, vol_window=51, thresh_old=2) {
 
 # convert time index to New_York
-  index(taq_data) <- with_tz(index(taq_data), "America/New_York")
+  index(taq_data) <- with_tz(time=index(taq_data), tzone="America/New_York")
 # subset data to NYSE trading hours
   taq_data <- taq_data['T09:30:00/T16:00:00', ]
 # return NULL if no data
   if (nrow(taq_data)==0)  return(NULL)
-  
+
 # remove duplicate time stamps using duplicated
   taq_data <- taq_data[!duplicated(index(taq_data)), ]
-  
+
+# select essential columns
+  taq_data <- taq_data[, c("Bid.Price", "Ask.Price", "Trade.Price", "Volume")]
+
 # scrub quotes with suspect bid-offer spreads
-  bid_offer <- taq_data[, 'Ask.Price'] - taq_data[, 'Bid.Price']
+  bid_offer <- taq_data[, "Ask.Price"] - taq_data[, "Bid.Price"]
 #  bid_offer <- na.omit(bid_offer)
   sus_pect <- extreme_values(bid_offer, vol_window=vol_window, thresh_old=thresh_old)
 # remove suspect values
@@ -270,19 +279,12 @@ scrub_TAQ <- function(taq_data, vol_window=51, thresh_old=2) {
 # calculate mid prices
   mid_prices <- 0.5 * (taq_data[, "Bid.Price"] + taq_data[, "Ask.Price"])
 #  mid_prices <- na.omit(mid_prices)
-  colnames(mid_prices) <- "Mid.Price"
-# replace suspect values with NA
-  mid_prices[jump_values(mid_prices)] <- NA
-  mid_prices <- na.locf(mid_prices)
-#  mid_prices <- na.locf(mid_prices, fromLast=TRUE)
-  mid_prices <- cbind(mid_prices, taq_data[index(mid_prices), "Volume"])
-  mid_prices[is.na(mid_prices[, "Volume"]), "Volume"] <- 0
-
-# aggregate to OHLC minutes data and cumulative volume
-  mid_prices <- to.period(x=mid_prices, period="minutes")
-# round up times to next minute
-  index(mid_prices) <- align.time(x=index(mid_prices), 60)
-  mid_prices
+#  colnames(mid_prices) <- "Mid.Price"
+# replace suspect values with NA, and perform 'locf'
+  taq_data[jump_values(mid_prices), ] <- NA
+  taq_data <- na.locf(taq_data)
+#  taq_data <- na.locf(taq_data, fromLast=TRUE)
+  taq_data
 }  # end scrub_TAQ
 
 
@@ -292,15 +294,26 @@ scrub_TAQ <- function(taq_data, vol_window=51, thresh_old=2) {
 #' Load, scrub, rbind, and aggregate multiple days of xts time series data for a single symbol.
 #' 
 #' @param sym_bol \code{character string} representing symbol or ticker.
-#' @param data_dir \code{character string} representing directory containing *.RData files.
+#' @param data_dir \code{character string} directory containing input *.RData files.
+#' @param output_dir \code{character string} directory containing output *.RData files.
 #' @return  \code{OHLC xts} time series.
-#' @details The high frequency data is assumed to be stored in separate directories for each symbol, containing *.RData files, each file with one day of data.  
-#' @details Each symbol has its own directory, and the symbol directories reside in 'data_dir'.
-#' @details The xts data is loaded, scrubbed, rbinded, and aggregated to OHLC format. 
-#' @details The OHLC data is assigned to a variable named 'sym_bol', and then saved to a *.RData file in the 'cwd'.
+#' @details The high frequency data is assumed to be stored in separate
+#'   directories for each symbol, containing *.RData files, each file with one
+#'   day of data.
+#' @details Each symbol has its own directory, and the symbol directories reside
+#'   in 'data_dir'.
+#' @details The xts data is loaded, scrubbed, rbinded, and aggregated to OHLC
+#'   format.
+#' @details The OHLC data is assigned to a variable named 'sym_bol', and then
+#'   saved to a *.RData file in the 'cwd'.
 #' @examples
+#' \dontrun{
 #' save_OHLC("SPY")
-save_OHLC <- function(sym_bol, data_dir="E:/mktdata/sec/", period="minutes") {
+#' }
+save_OHLC <- function(sym_bol, 
+                      data_dir="E:/mktdata/sec/", 
+                      output_dir="E:/output/data/",
+                      period="minutes") {
 
 # create path to directory containing *.RData files
   file_dir <- file.path(data_dir, sym_bol)
@@ -310,26 +323,26 @@ save_OHLC <- function(sym_bol, data_dir="E:/mktdata/sec/", period="minutes") {
   file_names <- file.path(file_dir, file_list)
 
 # load data into list
-  data <- sapply(file_names, function(file_name) {
+  data <- sapply(tail(file_names), function(file_name) {
     cat("loading", sym_bol, "frome file: ", file_name, "\n")
     data_name <- load(file_name)
     get(data_name)
   })
 
 # scrub and aggregate the data
-  data <- sapply(data, scrub_agg, period=period)
+  data <- lapply(data, scrub_agg, period=period)
 
 # recursively "rbind" the list into a single xts
   data <- do_call_rbind(data)
-# assign proper column names
-  colnames(data) <- sapply(strsplit(colnames(data), split="[.]"), 
+# assign column names, i.e. "symbol.High"
+  colnames(data) <- lapply(strsplit(colnames(data), split="[.]"), 
                            function(strng) paste(sym_bol, strng[-1], sep="."))
 
 # copy the xts data to a variable with the name 'sym_bol'
   assign(sym_bol, data)
 
-# save the xts data to a file in the 'cwd'
-  save(list=eval(sym_bol), file=paste0(sym_bol, ".RData"))
+# save the xts data to a file in the output_dir
+  save(list=eval(sym_bol), file=file.path(output_dir, paste0(sym_bol, ".RData")))
 
 }  # end save_OHLC
 
